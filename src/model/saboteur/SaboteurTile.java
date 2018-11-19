f54ae8d0285f733de26a11fd1ef1c3e29d3a2a2a
jco <julienco@protonmail.com>
2018-11-19 23:58:48 +0100

    Ajoute commentaires

--- src/model/saboteur/SaboteurTile.java
+++ src/model/saboteur/SaboteurTile.java
@@ -20,35 +20,42 @@ public abstract class SaboteurTile extends Tile {
     /**
      * Performs a Breadth-First Search on the graph structure
      * starting from the current instance of SaboteurTile
+     *
      * @return true if a treasure has been reached
      */
     public boolean treasureReached() {
-        // TODO comment the code
         // Queue of tiles to visit
-        Queue<SaboteurTile> queue = new ArrayDeque<>();
-        LinkedList<SaboteurTile> visitedTiles = new LinkedList<>();
-        queue.add(this);
-        while (!queue.isEmpty()) {
-            if (queue.peek() != null) {
-                SaboteurTile t = queue.poll();
-                //System.out.println(t);
-                System.out.println(t.getClass());
+        Queue<SaboteurTile> toVisit = new LinkedList<>();
+        // Queue of visited tiles
+        Queue<SaboteurTile> visitedTiles = new LinkedList<>();
+        toVisit.add(this); // Push current node to the queue
+        while (!toVisit.isEmpty()) {
+            /* Handle the case where the current tile is null
+             * This happens when a card has been removed from
+             * the board using a bomb card
+             */
+            if (toVisit.peek() != null) {
+                // Pop the next tile to visit
+                SaboteurTile t = toVisit.poll();
+                System.out.println(t.getClass()); // debug
 
                 if (t instanceof TreasureCard)
                     return true;
 
+                // This tile has already been visited, we skip it
                 if (visitedTiles.contains(t))
                     continue;
 
                 visitedTiles.add(t);
 
                 for (Side child : t.sides.values()) {
-                    if (child.getNextSide() == null) continue;
+                    if (child.getNextSide() == null)
+                        continue;
                     SaboteurTile childTile = (SaboteurTile) child.getNextSide().getParent();
-                    queue.add(childTile);
+                    toVisit.add(childTile);
                 }
             } else {
-                queue.remove();
+                toVisit.remove();
             }
         }
         return false;
