a4561316c0c5026bc4cb33340ffda83ffed8c48f
benichou <benichouyaniv@gmail.com>
2018-11-20 12:17:06 +0000

    creation de la classe

---
+++ ActionCard.java
@@ -0,0 +1,9 @@
+public class ActionCard extends Card{
+
+    public ActionCard(){
+	super.setCardName("ACTIONCARD");
+    }
+
+    public void use(Player p) //l'empeche de jouer une carte posable mais pas une carte action tant que personne n'as mis la contre action
+
+}
